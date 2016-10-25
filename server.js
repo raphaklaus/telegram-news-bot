d5afb7c9632fc72861905cc4571c963ac078ee6d
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-10-25 02:04:29 -0200

    feat: pretty much everything

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api'),
   bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true}),
   Promise = require('bluebird'),
+  request = require('request-promise'),
   feed = Promise.promisify(require('feed-read'));
 
-feedList = [{
+var feedList = [{
   site: 'Jovem Nerd',
   rss: 'http://jovemnerd.com.br/rss'
 }, {
@@ -16,24 +17,57 @@ feedList = [{
 }];
 
 var sites = feedList.map(item => [item.site]);
+var answerCallbacks = [];
 
 var options = {
   reply_markup: {
-    keyboard: sites
+    keyboard: sites,
+    'one_time_keyboard': true
   }
-}
+};
+
+bot.on('message', (msg) => {
+  let callback = answerCallbacks[msg.chat.id];
+  if (callback) {
+    delete answerCallbacks[msg.chat.id];
+    callback(msg);
+  }
+});
 
 console.log('I\'m alive!');
 
 bot.onText(/\/start/, (msg) => {
-  var fromId = msg.from.id;
-  bot.sendMessage(fromId, 'Please select the news source', options)
-    .then(choice => {
-      bot.onText(new RegExp(sites.join('|')), (msg, match) => {
-        feed({uri: feedList.find(obj => obj.site === match[0]).rss, encoding: 'utf8'}).then(articles => {
-          for (let article of articles)
-            console.log(article.title);
-        });
-      });
-    })
+  var chatId = msg.from.id;
+  bot.sendMessage(chatId, 'Please select the news source', options)
+    .then(() => {
+      answerCallbacks[chatId] = function(msg) {
+        console.log('entrou', msg);
+        return feed({uri: feedList.find(obj => obj.site === msg.text).rss,
+          encoding: 'utf8'})
+          .then(articles => {
+            console.log(articles);
+            let voice = [];
+            for (var i = 0; i < 3; i++) {
+              // Via text
+              // bot.sendMessage(chatId, articles[i].link);
+              voice.push(articles[i].title);
+            }
+
+            let voiceOptions = {
+              url: 'https://text-to-speech-demo.mybluemix.net/' +
+              'api/synthesize?voice=pt-BR_IsabelaVoice&text=' + encodeURI(voice.join('.\n').replace('|')),
+              encoding: null,
+              method: 'GET'
+            };
+
+            console.log('text:', voice.join('.\n').replace('|'));
+            console.log(voiceOptions.url);
+
+            return request(voiceOptions);
+          })
+          .then(file => {
+            bot.sendVoice(chatId, file);
+          });
+      };
+    });
 });
